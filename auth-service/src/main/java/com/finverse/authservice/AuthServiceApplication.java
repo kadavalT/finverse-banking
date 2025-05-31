2b16833ee67f543cb124f57bae49652776d9f87b
Kadavala Triveni <87759553+kadavalT@users.noreply.github.com>
2025-05-31 20:28:47 +0530

    ✨ Complete AuthService: JWT, roles, exception handling, logging

--- auth-service/src/main/java/com/finverse/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/finverse/authservice/AuthServiceApplication.java
@@ -2,9 +2,11 @@ package com.finverse.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableMethodSecurity(prePostEnabled = true)
 public class AuthServiceApplication {
 
